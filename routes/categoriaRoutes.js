c3d85d9dd99caa1011c72f27106ba99f428ec984
Mateus-Gonçalves <mateusdejesusg11@gmail.com>
2026-05-10 12:57:40 -0300

    Atualizar o categoriaRoutes.js

--- routes/categoriaRoutes.js
+++ routes/categoriaRoutes.js
@@ -2,9 +2,9 @@ const expresss = require('express');
 const router = expresss.Router();
 const categoriaController = require('../controllers/categoriaController');
 
-router.get('/categorias', CategoriaController.listarCategorias);
-router.post('/categorias', CategoriaController.criarCategoria);
-router.put('/categorias/:id', CategoriaController.atualizarCategoria);
-router.delete('/categorias/:id', CategoriaController.excluirCategoria);
+router.get('/categorias', categoriaController.listarCategorias);
+router.post('/categorias', categoriaController.criarCategoria);
+router.put('/categorias/:id', categoriaController.atualizarCategoria);
+router.delete('/categorias/:id', categoriaController.excluirCategoria);
 
 module.exports = router;
